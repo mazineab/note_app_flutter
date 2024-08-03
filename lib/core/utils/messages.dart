6787d5837619256b9728a18e0d5576f149cd63ea
mazineab <mazinabjlal@gmail.com>
2024-08-03 01:47:43 +0000

    messages

---
+++ lib/core/utils/messages.dart
@@ -0,0 +1,69 @@
+class Messages {
+  static const String Memo="Memo";
+
+  static const String success = "Success";
+  static const String error = "Error";
+  static const String ok = "Ok";
+  static const String no = "No";
+  static const String yes = "yes";
+  static const String save = "Save";
+  static const String cancel = 'Cancel';
+  static const String add = 'Add';
+  static const String request = "Request";
+  static const String remove = "Remove";
+  static const String edit = "Edit";
+
+
+  //category_controller
+  static const String startMsg = "Start creating a new category or note by pressing the + button";
+  static const String emptyCat = "This category currently has no notes. Please create one.";
+
+  static const String successAddCat = "The category has been added successfully";
+  static const String successDltCat = "Category deleted successfully";
+  static const String failDltCat = "An error occurred while deleting the category";
+  static const String errorAddCat = "Failed to add the category";
+
+  static const String failFetchNote = "Failed to fetch notes. Please try again later";
+  static const String successDltNote = "Note deleted successfully";
+  static const String failDltNote = "An error occurred while deleting the note";
+  static const String sucEdtNote = "Note edited successfully";
+  static const String failEdtNote = "An error occurred while editing the note";
+  static const String successAddNote="Note added successfully";
+  static const String failAddNote="An error occurred while adding the note";
+
+  //add_Npte_page
+  static const String enterNote="Enter your note here";
+  static const String chooseCat="Choose a category";
+  static const String nameNote="Name of the Note";
+  static const String enterNoteCont="Name of the Note";
+
+  //app_home_page
+  static const String wntDltCat="Do you want to delete this category?";
+  static const String createNt="Create a new note";
+  static const String createCat="Create a new note";
+  static const String catName="Category name";
+  static const String edtNt="Edit Note";
+  static const String svChng="Save changes";
+  static const String wntDltNt="Do you want to delete this note ?";
+
+  //user_home_page
+  static const String wlc="Welcome back!";
+  static const String wlcMsg="Welcome to Memo! Sign in to your existing account or create a new one to get started.";
+  static const String login="Login";
+  static const String register="register";
+  static const String username="Username";
+  static const String email="Email";
+  static const String password="Password";
+  static const String phNbr="Phone Number";
+  static const String signin="Sign In";
+  static const String signup="Sign Up";
+
+  //user_controller
+  static const String emlOrPss="Email or password is incorrect";
+  static const String successAcc="Your account has been created successfully";
+  static const String failAcc="An error occurred while creating the account";
+
+  //firt_home
+  static const String desc="Save your notes online and locally without worrying about losing them.";
+
+}
